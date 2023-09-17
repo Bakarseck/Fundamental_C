f7a17b7d6970603f9aa799fce871cb1ae96dc249
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2023-09-17 14:39:02 +0000

    dice

---
+++ dice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DiceRollGame
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RollButton_Click(object sender, EventArgs e)
+        {
+            // Générer un nombre aléatoire entre 1 et 6 (inclus)
+            int result = random.Next(1, 7);
+
+            // Afficher le résultat
+            resultLabel.Text = "Résultat : " + result.ToString();
+        }
+    }
+}
+
