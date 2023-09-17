4acdc28aafc81288d15a6ef3c86b3e5c152e6ed2
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2023-09-17 16:48:11 +0000

    earn certifcations

---
+++ deboguages/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+Random random = new Random();
+int roll1 = random.Next(0, 10);
+
+for (int i = 0; i < 10; i++)
+{
+    int roll = random.Next(0, 10);
+    if (roll == roll1) {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+    } else {
+        Console.BackgroundColor = ConsoleColor.Red;
+    }
+    Console.WriteLine($"{roll}");
+}
